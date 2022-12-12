d50a17ca8c16e0b914f33fa10457dbae1a09f1df
Lucas Cortes <luc.cortes@duocuc.cl>
2022-12-12 00:04:47 -0300

    Api lista

---
+++ apiet/src/models/users.model.ts
@@ -0,0 +1,39 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Users extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  user: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  pass: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Users>) {
+    super(data);
+  }
+}
+
+export interface UsersRelations {
+  // describe navigational properties here
+}
+
+export type UsersWithRelations = Users & UsersRelations;
